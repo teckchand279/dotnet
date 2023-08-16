b8ab12dad696620eb726f31965889e0d0460860c
teckchand279 <133816186+teckchand279@users.noreply.github.com>
2023-08-17 00:36:33 +0530

    Add files via upload

---
+++ BOL/Employee.cs
@@ -0,0 +1,35 @@
+﻿namespace BOL_poco_
+{
+    public class Employee
+    {
+        private int id;
+        private string name;
+        private double salary;
+        public Employee() { }
+        public Employee(int id, string name, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public double Salary
+        {
+            get { return this.salary; } set { this.salary = value; }
+        }
+        public override string ToString()
+        {
+            string data=String.Format("Id={0},Name={1},Salary={2}",this.id,this.name,this.salary);
+            return data;
+        }
+    }
+}
